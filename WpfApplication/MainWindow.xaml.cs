301eb8a4534e0070f692d28ca00b8792ee22404c
rahul <rahulsalinskee@gmail.com>
2024-05-17 19:16:47 +0530

    Initial Commit

---
+++ WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToRed_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["DynamicColor"] = new SolidColorBrush(Colors.Red);
+        }
+
+        private void ToBlue_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["DynamicColor"] = new SolidColorBrush(Colors.Blue);
+        }
+    }
+}
